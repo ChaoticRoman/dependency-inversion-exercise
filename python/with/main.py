81a891b60ba3a92596d29e20d0f77e22f3018b32
Roman Pavelka <roman.pavelka@siemens.com>
2023-05-23 20:16:36 +0200

    Improve type safety

--- python/with/main.py
+++ python/with/main.py
@@ -1,9 +1,10 @@
+from read_users import UserReader
 from read_users_from_file import FileUserReader
 
 if __name__ == "__main__":
     username = input("username: ").strip()
 
-    reader = FileUserReader()
+    reader: UserReader = FileUserReader()
     if username in reader.read_users():
         print("Logged in.")
     else:
